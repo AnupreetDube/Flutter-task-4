ffecfbeba272edca70ac0f0dba152dd957c1d610
Anupreet <64468290+AnupreetDube@users.noreply.github.com>
2021-05-26 21:20:43 +0530

    Create API.py

---
+++ API.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import cgi
+import subprocess
+print("content-type: text/html")
+print()
+mydata = cgi.FieldStorage()
+command = mydata.getvalue("y")
+output = subprocess.getoutput("sudo " + command)
+print(output)
